a3964d5abf6eb6e1ba0724060fae7db93a165aea
enkov <alexey.enkov@gmail.com>
2017-03-24 21:48:42 +0300

    refactoring & add meta viewport

--- script/leaflet.js
+++ script/leaflet.js
@@ -3,6 +3,7 @@ if (typeof kotlin === 'undefined') {
 }
 var leaflet = function (_, Kotlin) {
   'use strict';
+  var HashSet_init = Kotlin.kotlin.collections.HashSet_init_287e2$;
   function Content(input, button) {
     this.input = input;
     this.button = button;
@@ -34,30 +35,18 @@ var leaflet = function (_, Kotlin) {
     return this === other || (other !== null && (typeof other === 'object' && (Object.getPrototypeOf(this) === Object.getPrototypeOf(other) && (Kotlin.equals(this.input, other.input) && Kotlin.equals(this.button, other.button)))));
   };
   function main$lambda(it) {
-    init();
+    return new App();
   }
   function main(args) {
     window.onload = main$lambda;
   }
-  function init$ObjectLiteral() {
-    this.attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors';
+  function App() {
+    this.map_0 = null;
+    var content = this.generateContent();
+    this.map_0 = new Leaflet('mapId');
+    content.button.onclick = App_init$lambda(content, this);
   }
-  init$ObjectLiteral.$metadata$ = {
-    kind: Kotlin.Kind.CLASS,
-    interfaces: []
-  };
-  function init$lambda(closure$content, closure$map) {
-    return function (it) {
-      query(closure$content.input.value, closure$map);
-    };
-  }
-  function init() {
-    var content = generateContent();
-    var map = L.map('mapId').setView([55.85, 38.46], 13);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', new init$ObjectLiteral()).addTo(map);
-    content.button.onclick = init$lambda(content, map);
-  }
-  function generateContent() {
+  App.prototype.generateContent = function () {
     var tmp$, tmp$_0, tmp$_1, tmp$_2;
     var mapDiv = Kotlin.isType(tmp$ = document.createElement('div'), HTMLElement) ? tmp$ : Kotlin.throwCCE();
     mapDiv.id = 'mapId';
@@ -75,53 +64,73 @@ var leaflet = function (_, Kotlin) {
     body.appendChild(input);
     body.appendChild(buttonDiv);
     return new Content(input, buttonDiv);
-  }
-  function query$lambda(it) {
-    showText('before json parse');
-    return it.json();
-  }
-  function query$lambda_0(closure$map) {
+  };
+  function App$query$lambda(this$App) {
     return function (it) {
-      showText('after json parse');
+      this$App.showText_61zpoe$('before json parse');
+      return it.json();
+    };
+  }
+  function App$query$lambda_0(this$App, closure$map) {
+    return function (it) {
+      this$App.showText_61zpoe$('after json parse');
       var elements = it.elements;
-      showText('after elements to array');
+      this$App.showText_61zpoe$('after elements to array');
       var tmp$;
       for (tmp$ = 0; tmp$ !== elements.length; ++tmp$) {
         var element = elements[tmp$];
-        var closure$map_0 = closure$map;
-        L.marker([element.center.lat, element.center.lon]).addTo(closure$map_0);
+        var tmp$_0, tmp$_1, tmp$_2, tmp$_3;
+        closure$map.addMarker_lu1900$((tmp$_1 = element.lat) != null ? tmp$_1 : ((tmp$_0 = element.center) != null ? tmp$_0 : Kotlin.throwNPE()).lat, (tmp$_3 = element.lon) != null ? tmp$_3 : ((tmp$_2 = element.center) != null ? tmp$_2 : Kotlin.throwNPE()).lon);
       }
-      showText('after marker');
+      this$App.showText_61zpoe$('after marker');
     };
   }
-  function query$lambda_1(it) {
-    showText(it.toString());
+  function App$query$lambda_1(this$App) {
+    return function (it) {
+      this$App.showText_61zpoe$(it.toString());
+    };
   }
-  function query(number, map) {
-    showText('query begin');
-    var bounds = getBounds(map);
-    showText('bounds');
-    var query_0 = '[out:json];(' + ('node[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ('way[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ('rel[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ');out center;';
-    showText('beforeFetch');
-    if (!window.fetch) showText("window.fetch missing")
-    window.fetch('https://overpass-api.de/api/interpreter?data=' + query_0).then(query$lambda).then(query$lambda_0(map)).catch(query$lambda_1);
-    showText('afterFetch');
-  }
-  function getBounds(map) {
+  App.prototype.query_bws5j5$ = function (number, map) {
+    this.showText_61zpoe$('query begin');
+    this.clear();
     var bounds = map.getBounds();
-    return '(' + bounds._southWest.lat + ',' + bounds._southWest.lng + ',' + bounds._northEast.lat + ',' + bounds._northEast.lng + ')';
-  }
-  function showText(s) {
+    this.showText_61zpoe$('bounds');
+    var query = '[out:json];(' + ('node[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ('way[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ('rel[' + '"' + 'addr:housenumber' + '"' + '=' + '"' + number + '"' + ']') + (bounds + ';') + ');out center;';
+    this.showText_61zpoe$('beforeFetch');
+    window.fetch('https://overpass-api.de/api/interpreter?data=' + query).then(App$query$lambda(this)).then(App$query$lambda_0(this, map)).catch(App$query$lambda_1(this));
+    this.showText_61zpoe$('afterFetch');
+  };
+  App.prototype.showText_61zpoe$ = function (s) {
     var tmp$, tmp$_0;
     var text = Kotlin.isType(tmp$ = document.createElement('div'), HTMLDivElement) ? tmp$ : Kotlin.throwCCE();
     text.className = 'text';
     text.innerText = s;
     ((tmp$_0 = document.body) != null ? tmp$_0 : Kotlin.throwNPE()).appendChild(text);
+  };
+  App.prototype.clear = function () {
+    var tmp$;
+    var c = document.getElementsByClassName('text');
+    while (c.length > 0) {
+      ((tmp$ = c[0]) != null ? tmp$ : Kotlin.throwNPE()).remove();
+    }
+    this.map_0.clearAllMarkers();
+  };
+  function App_init$lambda(closure$content, this$App) {
+    return function (it) {
+      this$App.query_bws5j5$(closure$content.input.value, this$App.map_0);
+    };
   }
-  function OSMElement(type, id, center) {
+  App.$metadata$ = {
+    kind: Kotlin.Kind.CLASS,
+    simpleName: 'App',
+    interfaces: []
+  };
+  function OSMElement(type, id, center, lat, lon) {
     this.type = type;
     this.id = id;
     this.center = center;
+    this.lat = lat;
+    this.lon = lon;
   }
   OSMElement.$metadata$ = {
     kind: Kotlin.Kind.CLASS,
@@ -137,19 +146,51 @@ var leaflet = function (_, Kotlin) {
     simpleName: 'Coord',
     interfaces: []
   };
+  function Leaflet(mapId) {
+    this.map_0 = L.map(mapId).setView([55.85, 38.46], 13);
+    this.markers_0 = null;
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', new Leaflet_init$ObjectLiteral()).addTo(this.map_0);
+    this.markers_0 = HashSet_init();
+  }
+  Leaflet.prototype.getBounds = function () {
+    var bounds = this.map_0.getBounds();
+    return '(' + bounds._southWest.lat + ',' + bounds._southWest.lng + ',' + bounds._northEast.lat + ',' + bounds._northEast.lng + ')';
+  };
+  Leaflet.prototype.addMarker_lu1900$ = function (lat, lon) {
+    var marker = L.marker([lat, lon]);
+    marker.addTo(this.map_0);
+    this.markers_0.add_11rb$(marker);
+  };
+  Leaflet.prototype.clearAllMarkers = function () {
+    var tmp$;
+    tmp$ = this.markers_0.iterator();
+    while (tmp$.hasNext()) {
+      var element = tmp$.next();
+      element.remove();
+    }
+  };
+  function Leaflet_init$ObjectLiteral() {
+    this.attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors';
+  }
+  Leaflet_init$ObjectLiteral.$metadata$ = {
+    kind: Kotlin.Kind.CLASS,
+    interfaces: []
+  };
+  Leaflet.$metadata$ = {
+    kind: Kotlin.Kind.CLASS,
+    simpleName: 'Leaflet',
+    interfaces: []
+  };
   var package$ru = _.ru || (_.ru = {});
   var package$spb = package$ru.spb || (package$ru.spb = {});
   var package$enkov = package$spb.enkov || (package$spb.enkov = {});
   var package$leaflet = package$enkov.leaflet || (package$enkov.leaflet = {});
   package$leaflet.Content = Content;
   package$leaflet.main_kand9s$ = main;
-  package$leaflet.init = init;
-  package$leaflet.generateContent = generateContent;
-  package$leaflet.query_bm4g0d$ = query;
-  package$leaflet.getBounds_za3rmp$ = getBounds;
-  package$leaflet.showText_61zpoe$ = showText;
+  package$leaflet.App = App;
   package$leaflet.OSMElement = OSMElement;
   package$leaflet.Coord = Coord;
+  package$leaflet.Leaflet = Leaflet;
   Kotlin.defineModule('leaflet', _);
   main([]);
   return _;
